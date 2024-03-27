e7cac9be5c82d47b9a09928b958702b4936e5f10
razzaksr <razzaksr@gmail.com>
2024-03-27 06:18:00 +0530

    Spring boot security in-memory level2

--- src/main/java/elements/spring/explore/security/BankersSecurity.java
+++ src/main/java/elements/spring/explore/security/BankersSecurity.java
@@ -6,7 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 public class BankersSecurity {
@@ -15,6 +22,14 @@ public class BankersSecurity {
     BankersService service;
 
     AuthenticationManager authenticationManager;
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails userDetails1= User.withDefaultPasswordEncoder().username("razak").password("mohamed").roles("admin").build();
+        UserDetails userDetails2= User.withDefaultPasswordEncoder().username("rasheedha").password("mohamed").roles("manager").build();
+        List<UserDetails> userDetailsList= Stream.of(userDetails1,userDetails2).collect(Collectors.toList());
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
 
 //    @Bean
 //    public PasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();}
